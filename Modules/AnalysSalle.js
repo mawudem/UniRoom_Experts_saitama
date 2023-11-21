f58c79311fc3d93c054eb85bdab50a1d6565c78f
Nicolas <130666872+Nico6003@users.noreply.github.com>
2023-11-21 18:09:51 +0100

    Update AnalysSalle.js

--- Modules/AnalysSalle.js
+++ Modules/AnalysSalle.js
@@ -1,6 +1,35 @@
 class AnalysSalle
 {
 
+    constructor(liste_salle){
+        this.liste_salle=liste_salle; //tableau de salles
+    }
+
+    //retourne l'index de la salle dans liste_salle
+    get_salle_index(salle) {
+        let index;
+        for(let i=0; i<this.liste_salle.length();i++){
+            if(this.liste_salle[i]==salle.salle_id){
+                index=i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    //verifie la disponiblite d'une salle pour un jour et une heure donnée
+    is_disponible(salle, jour, heure) {
+        let libre = new Boolean(true);
+        if (salle.sessions[jour][i] != 0) {
+            libre = false;
+        }
+        return libre;
+    }
+
+    //recupere la capacité de la salle
+    get_capacite(salle) {
+        return salle.getNbPlaces();
+    }
 }
 
 module.exports = AnalysSalle;
