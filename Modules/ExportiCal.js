42a212f1e10d1b0b204ca635670205403261799d
Nico6003 <130666872+Nico6003@users.noreply.github.com>
2023-12-06 22:32:36 +0100

    Verification saisie utilisateur

--- Modules/ExportiCal.js
+++ Modules/ExportiCal.js
@@ -10,27 +10,34 @@ class ExportiCal {
         this.cal = iCal({ name: 'Timetable' }); //création du calendrier
     }
 
+    //permet de verifier si une date est bien au format DD/MM/YYYY
+    dateValide(input) {
+        return /^\d{2}\/\d{2}\/\d{4}$/.test(input);
+    }
 
+    //permet de vérifier que l'on saisie des chiffres
+     numValide(input) {
+        return /^\d+$/.test(input) && input.length > 0;
+    }
+    
 
     //trouve le cours dans la liste des UE
     trouver_cours_dliste(nom) {
-        //console.log(this.UE_Liste);
         let i = 0;
-        while (this.UE_Liste[i].get_identifiant() != nom) {
-            i++;
-            if (i > this.UE_Liste.length) {
-                console.error(`Impossible de trouver : `, nom);
-                return false;
+        while (i < this.UE_Liste.length) {
+            if (this.UE_Liste[i].get_identifiant() === nom) {
+                console.log(this.UE_Liste[i]);
+                return this.UE_Liste[i];
             }
+            i++;
         }
-        console.log(this.UE_Liste[i]);
-        return this.UE_Liste[i];
+        console.error(`Impossible de trouver : `, nom);
+        return false;
     }
+
 
     //Génération du iCalendar
     est_acceptable(heure_debut, minute_debut, heure_fin, minute_fin) {
-        console.log(`Heure de début : ${heure_debut}:${minute_debut}`);
-        console.log(`Heure de fin : ${heure_fin}:${minute_fin}`);
         if ((heure_debut >= 8) && (heure_fin + (minute_fin / 60) <= 20)) //début et fin respectant les limites
         {
             if (heure_debut + (minute_debut / 60) + 1 < heure_fin + (minute_fin / 60)) // horraires cohérents
@@ -43,12 +50,11 @@ class ExportiCal {
     //Ajout d'un cours dans le calendrier
     Ajout_evenement(annee, mois, jour, heure_debut, minute_debut, heure_fin, minute_fin, titre, salle) {
         if (this.est_acceptable(heure_debut, minute_debut, heure_fin, minute_fin)) {
-            console.log("On ajoute le cours : " + mois, jour, titre)
 
             this.cal.createEvent({
-                start: new Date(annee, mois, jour, heure_debut , minute_debut, 0, 0, 0),
-                end: new Date(annee, mois, jour, heure_fin , minute_fin, 0, 0, 0),
-                summary : titre,
+                start: new Date(annee, mois, jour, heure_debut, minute_debut, 0, 0, 0),
+                end: new Date(annee, mois, jour, heure_fin, minute_fin, 0, 0, 0),
+                summary: titre,
                 location: salle,
                 time_zone: 'Europe/London',
             });
@@ -67,7 +73,16 @@ class ExportiCal {
             let currentUE = this.trouver_cours_dliste(input);
             if (!currentUE === false) {
                 currentUE.toString();
-                input = prompt("Entrez les numéros correspondant à vos créneaux (comme 134 pour 1 3 et 4) : "); //Choisir les sessions
+                let imput = '';
+                //vérifier la saisie
+                do {
+                    input = prompt("Entrez les numéros correspondant à vos créneaux (ex: 123) : ");
+                    if (!this.numValide(input)) {
+                        console.log("Saisie incorrecte. Veuillez entrer uniquement une série de chiffres.");
+                    }
+                } while (!this.numValide(input));
+
+                //input = prompt("Entrez les numéros correspondant à vos créneaux (comme 134 pour 1 3 et 4) : "); //Choisir les sessions
                 for (let i = 0; i < input.length; i++) //Ajouter les bons dans la liste des exports
                 {
                     if (typeof (parseInt(input[i])) === "number") {
@@ -78,33 +93,46 @@ class ExportiCal {
             }
         };
         //Choix de la période d'export
-        let debut_exportation_jour = prompt("Enter the begining date of the export period (format DD/MM/YYYY): ");
-        let fin_exportation_jour = prompt("Enter the ending date of the export period (format DD/MM/YYYY): ");
-        console.log(debut_exportation_jour,fin_exportation_jour);
-        console.log(parseInt(debut_exportation_jour.split('/')[2]));
+        let debut_exportation_jour = '';
+        let fin_exportation_jour = '';
+        //verifier la saisie de l'utilisateur
+        do {
+            debut_exportation_jour = prompt("Entrez la date de début de la période a exporter (format DD/MM/YYYY): ");
+            if (!this.dateValide(debut_exportation_jour)) {
+                console.log("Saisie invalide. Entrez une date au format DD/MM/YYYY.");
+            }
+        } while (!this.dateValide(debut_exportation_jour));
+
+        do {
+            fin_exportation_jour = prompt("Entrez la date de fin de la période a exporter (format DD/MM/YYYY): ");
+            if (!this.dateValide(fin_exportation_jour)) {
+                console.log("Saisie invalide. Entrez une date au format DD/MM/YYYY.");
+            }
+        } while (!this.dateValide(fin_exportation_jour));
+
+
         let debut_date = new Date(
             parseInt(debut_exportation_jour.split('/')[2]),
-            parseInt(debut_exportation_jour.split('/')[1])-1 ,
-            parseInt(debut_exportation_jour.split('/')[0])+1
+            parseInt(debut_exportation_jour.split('/')[1]) - 1,
+            parseInt(debut_exportation_jour.split('/')[0]) + 1
         );
 
         let fin_date = new Date(
             parseInt(fin_exportation_jour.split('/')[2]),
-            parseInt(fin_exportation_jour.split('/')[1])-1,
-            parseInt(fin_exportation_jour.split('/')[0])+1
+            parseInt(fin_exportation_jour.split('/')[1]) - 1,
+            parseInt(fin_exportation_jour.split('/')[0]) + 1
         );
 
-        console.log(debut_date,fin_date);
         let nb_jours_exportation = Math.round(Math.abs(fin_date - debut_date) / (1000 * 60 * 60 * 24));
-        console.log(nb_jours_exportation);
+
         for (let i = 0; i < nb_jours_exportation; i++) {
             let jour = new Date(debut_date.getFullYear(), debut_date.getMonth(), debut_date.getDate() + i, 0, 0);
             let cours_du_jour = this.cours_a_exporter;
 
             if (cours_du_jour.length > 0) {
                 //for (let j = 0; j < cours_du_jour.length; j++) {
-                    console.log("Exportation en cours....");
-                    cours_du_jour.forEach(session => this.Ajout_evenement(parseInt(jour.getFullYear()), parseInt(jour.getMonth()), parseInt(jour.getDay()), parseInt(session.get_h_debut()), parseInt(session.get_min_debut()), parseInt(session.get_h_fin()), parseInt(session.get_min_fin()), session.get_sommaire(), session.get_Salle()));
+                console.log("Exportation en cours....");
+                cours_du_jour.forEach(session => this.Ajout_evenement(parseInt(jour.getFullYear()), parseInt(jour.getMonth()), parseInt(jour.getDate()), parseInt(session.get_h_debut()), parseInt(session.get_min_debut()), parseInt(session.get_h_fin()), parseInt(session.get_min_fin()), session.get_sommaire(), session.get_Salle()));
                 //}
             }
         }
